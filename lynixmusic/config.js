3a10981b4e48dc3d5621673b681d8eccaa2987c9
GltichDev <85330326+GltichDev@users.noreply.github.com>
2021-06-04 20:54:12 +0530

    Add files via upload

---
+++ lynixmusic/config.js
@@ -0,0 +1,8 @@
+/* Your Bot Default Prefix For Every Server */
+exports.Default_Prefix = "?";
+/* Your Bot All Embeds Color */
+exports.Color = "#f8f614";
+/* Your Bot Token (Secret) */
+exports.Token = "ODQ2MjY0NzIwODQ3MDExODgw.YKs_cA.F9O7ImewLqOI1OfKqguDbSUrKa8";
+/* Bot's Owner ID */
+exports.OwnerID = "599255216201924629";
